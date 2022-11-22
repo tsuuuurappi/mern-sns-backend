f2119078fb0d9a42229f49ca79f43b864d65f84a
iamtsukanoshin <iamtsukanoshin@gmail.com>
2022-11-22 12:21:05 +0900

    add route-setting

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+const PORT = 3000;
+
+// ミドルウェア
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
+
+// ルーティング
+// app.get('/', (req, res) => {
+//   res.send('hello express');
+// });
+
+// app.get('/users', (req, res) => {
+//   res.send('users express');
+// });
+
+// サーバを起動
+app.listen(PORT, () => console.log('サーバーが起動しました'));
