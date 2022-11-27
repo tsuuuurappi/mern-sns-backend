418216aa7bc29680b1df41eae69e79a78fc5cedb
iamtsukanoshin <iamtsukanoshin@gmail.com>
2022-11-27 23:02:51 +0900

    add upload-image + refactoring

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+const uploadRoute = require('./routes/upload');
 const PORT = 5000;
 const mongoose = require('mongoose');
+const path = require('path');
 // dotenvライブラリの利用(process.env.xxxで.envの設定値を利用できる)
 require('dotenv').config();
 
@@ -19,10 +21,12 @@ mongoose
   });
 
 // ミドルウェア
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
+app.use('/api/upload', uploadRoute);
 
 // ルーティング
 app.get('/', (req, res) => {
